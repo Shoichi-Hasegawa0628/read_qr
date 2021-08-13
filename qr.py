0171a366d9a4453be396422ac2c2f568a397fd44
HasegawaShoichi <0ve38x163040p4z@ezweb.ne.jp>
2021-08-13 17:07:11 +0900

    Created read_qr source code

---
+++ qr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 以下のモジュールが必要 (QRコード作成用)
+# pip install pyzbar
+# pip install qrcode
+# apt-get install python3-zbar
+
+
+import qrcode
+import cv2
+from PIL import Image
+from pyzbar.pyzbar import decode
+
+# QRコードの作成
+qr = qrcode.QRCode(box_size=2)
+qr.add_data('Hello')
+qr.make()
+img_qr = qr.make_image()
+img_qr.save('./data/test.png')
+#cv2.imwrite('./data/test.png', img_qr)
+
+#QRコードを読み込む
+im = cv2.imread('./data/test.png')
+data = decode(im)
+input=data[0][0].decode('utf-8', 'ignore')
+print(input)
+
